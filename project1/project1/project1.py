0c0063c6828336b4b1f00d8f49ae0722a35fdb70
ConnorCox1996 <ccox10@ur.rochester.edu>
2018-10-05 15:02:04 -0400

    added the working function and its test function to the package

--- project1/project1/project1.py
+++ project1/project1/project1.py
@@ -1,3 +1,183 @@
-# -*- coding: utf-8 -*-
+#Project 1 
+#Import packages: numpy, scipy,& math packages 
+import numpy as np
+import scipy.constants
+import math
+import matplotlib.pyplot as plt
+#Import argparse
+import argparse
 
-"""Main module."""
+# "ref" function parses arguments from command line
+#saves input temperature, total time, timestep, intial position & velocity inptuts as variables
+def ref():
+    #input parameters using argparse
+    parser = argparse.ArgumentParser(description='arguments')
+    parser.add_argument('-T', '--temperature', metavar='temperature', type=float,  default=1, nargs='?', help=' ')
+    parser.add_argument('-tt', '--total_time', metavar='total_time', type=float, default=10, nargs='?', help=' ')
+    parser.add_argument('-ts', '--time_step', metavar='time_step', type=float, default=1, nargs='?', help=' ')
+    parser.add_argument('-ip','--initial_position', metavar='initial_position', type=float, default=1, nargs='?', help=' ')
+    parser.add_argument('-iv', '--initial_velocity', metavar='initial_velocity', type=float, default=1, nargs='?', help=' ')
+    parser.add_argument('-d', '--damping_coefficient', metavar='damping_coefficient', type=float, default=1, nargs='?', help=' ')
+    
+    args = parser.parse_args()
+    temperature=args.temperature
+    totaltime=args.total_time
+    timestep=args.time_step
+    initialposition=args.initial_position
+    initialvelocity=args.initial_velocity
+    gamma=args.damping_coefficient
+    
+    return temperature,totaltime,timestep,initialposition,initialvelocity,gamma
+
+
+#Set input values equal to Variables
+temperature,totaltime,timestep,initialposition,initialvelocity,gamma=ref()
+Temperature=temperature
+Gamma=gamma
+TimeStep=timestep
+TotalTime=totaltime
+InitialPosition=initialposition
+InitialVelocity=initialvelocity
+
+#num_steps calculates the total number of steps taken 
+#calculated from total time and timestep inputs
+def num_steps(TotalTime, TimeStep):
+        number_steps= TotalTime / TimeStep
+        return number_steps
+number_Steps=num_steps(TotalTime,TimeStep)
+Number_Steps=int(number_Steps)
+#                                           CHECKER
+#print("NUMBERSTEPS")
+#print(Number_Steps)
+
+#Solve for Standard Deviation
+#finds Standard deviation, stored as "sigma", from temperature & gamma inputs 
+#boltzmann constant is in reduced units, =1
+boltzmann_constant=1
+#Temperature=int(args.temperature)
+def std(Temperature, Gamma):
+    sig= math.sqrt(2 * Temperature * Gamma * boltzmann_constant)
+    return sig
+
+sigma=std(Temperature,Gamma)
+
+#using the standard deviation calculated above,
+#random force is simulated by distribution of normally distributed random numbers
+#this distribution models the random force because the standard deviation of the distribution is equal to the
+#square root of the variance between random force at time=t & time=t+1
+def rand_num_gen(mu, Sigma, Number_Steps):
+    xi= np.random.normal(mu, Sigma, Number_Steps)
+    return xi
+
+dist=rand_num_gen(0,sigma,Number_Steps)
+
+#The random force can be used in the position, velocity, and acceleration integrator
+#Create lists for position & velocity iterations starting with their initial values (@t=0)
+#posi=[InitialPosition]
+#vel= [InitialVelocity]
+#specify mass
+mass=1
+# Calculate initial acceleration, according to Langevin equation 
+def initial_acceleration(distro):
+    a0=((1/mass)*((-Gamma*InitialVelocity)+distro[0]))
+    return a0
+InitialAcceleration=initial_acceleration(dist)
+#Create list for acceleration iterations starting with initial acceleration value (@t=0)
+#acc=[InitialAcceleration]
+
+#Iterate position, velocity and acceleration at each timestep
+#Walls at position of 0 and 5 limit the particle movement
+finalpos=[]
+def particle_motion(Number_Steps):
+    posi=[InitialPosition]
+    vel= [InitialVelocity]
+    acc=[InitialAcceleration]
+    dist=rand_num_gen(0,sigma,Number_Steps)
+    for i in range(1, Number_Steps, 1):
+       
+        vadd=(vel[i-1]+(acc[i-1]*TimeStep))
+        vel.append(vadd)
+        padd=posi[i-1]+(vel[i-1]*(TimeStep))
+        posi.append(padd)
+        aadd=(1/mass)*((-gamma*vel[i])+(dist[i]))
+        acc.append(aadd)
+        if posi[i] < 0:
+            break
+        elif posi[i] >= 5.0:
+            break
+    steps_taken_at_collision=(i*0.1)
+    
+   
+
+    
+    #                                   CHECKERS
+    #print(posi)
+    #print("Position at run")
+    #print("stepstaken")
+    #print(steps_taken_at_collision)
+    return steps_taken_at_collision, posi, vel
+
+
+test1=particle_motion(10)
+print("Final Position")
+print(test1[1][-1])
+print("Final Velocity")
+print(test1[2][-1])
+
+
+
+
+
+
+
+#multi-run simulator
+def multi_run_simulator(runs):
+    steps_at_collision_for_each_run=[]
+    for j in range(runs):
+        r=particle_motion(Number_Steps)
+        
+        #print(run_iteration)
+        #print("next")
+        steps_at_collision_for_each_run.append(r[0])
+       
+    return steps_at_collision_for_each_run
+    #print(steps_at_collision_for_each_run)
+
+    #TEST   
+test2=multi_run_simulator(100)
+
+#                                       CHECKERS
+# print("run result")    
+#print(test2)
+    
+
+def hist_generator(data):
+    bins=[0,0.25,0.5,0.75,1,1.25,1.5,1.75,2.0]
+    plt.hist(data, bins)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+    #TEST
+test3=hist_generator(test2)
+
+
+def write_output(steps_taken_at_collision, posi, vel,TimeStep):
+    
+    header = "#Index    Time      Position      Velocity"
+    #make a text file called "Output"
+    Output = open('Output.txt', "w")
+    Output.write(header)
+    Output.write("\n")
+    #except for the header, it writes index,time,position line by line
+    
+    for i in range(len(posi)):
+        Output.write('{}            {}            {:.2f}              {:.2f}'.format(i, (i*TimeStep), posi[i], vel[i]))
+        Output.write('\n')
+    Output.close()
+
+
+steps_at_collision_for_each_run, posi, vel=particle_motion(Number_Steps)
+write_output(steps_at_collision_for_each_run,posi,vel,TimeStep)
+
+
--- project1/tests/test_project1.py
+++ project1/tests/test_project1.py
@@ -1,25 +1,99 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Tests for `project1` package."""
-
+#Test Project 1
+import unittest
 import pytest
+import function1
+
+import numpy as np
+
+#Can't get parser to work...
+#class Testref(unittest.TestCase):
+    #def test_ref(self):
+        #self.parser = NewProj1.ref()
+        #parsed = self.parser.parse_args(['--temperature', '300.0','--total_time', '1000.0','--time_step', '0.1','--initial_position', '0.0','--initial_velocity', '0.0','--damping_coefficient', '0.1'])
+        #self.assertEqual([parsed.temperature,parsed.total_time, parsed.time_step, parsed.initial_position, parsed.initial_velocity,   parsed.damping_coefficient], [[300.0], [1000.0], [0.1], [0.0], [0.0], [0.1]] )
+#if __name__ == '__main__':
+    #unittest.main()
 
 
-from project1 import project1
+
+class TestNumSteps(unittest.TestCase):
+    def test_num_steps(self):
+        TotalTime=10
+        TimeStep=1
+        self.assertEqual(NewProj1.num_steps(TotalTime,TimeStep),10.0)
+ 
+
+class TestStd(unittest.TestCase):
+    def test_std(self):
+        self.assertAlmostEqual(NewProj1.std(1,1),1.4142135623730951)
+ 
+
+class TestRandNumGen(unittest.TestCase):
+    def test_rand_rum_gen(self):
+        distr=NewProj1.rand_num_gen(0,NewProj1.sigma,10)
+        dif_in_means=abs(0-np.mean(distr))
+        self.assertLess(abs(0-np.mean(NewProj1.rand_num_gen(0,0.1,10))),.05)
+        dif_in_variance=NewProj1.sigma-np.std(distr, ddof=1)
+        self.assertLess(dif_in_variance,0.05)
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+class TestInitialAcceleration(unittest.TestCase):
+    def test_initial_acceleration(self):
+        distr=[2,1,3]
+        self.assertEqual(NewProj1.initial_acceleration(distr),1)
+#if __name__ == '__main__':
+    #unittest.main()
+
+#class TestParticleMotion(unittest.TestCase):
+    #def test_particle_motion(self):
+    #    posi=[0]
+    #    vel=[1]
+    #    acc=[0]
+    #    dist=[1,1,1]
+    #    Number_Steps=2
+    #    NewProj1.particle_motion(Number_Steps)
+    #    print(posi)
+    #    self.assertEqual(posi,[0,1,2])
+#if __name__ == '__main__':
+#    unittest.main()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+#ewProj1.particle_motion(3)
+#print(posi)
+
+
+
+def iteration_until_particle_collision_with_wall(runs,Number_Steps,store_place):
+    for j in range (0,runs):
+        dist=rand_num_gen(0,sigma,Number_Steps)
+        place=[]
+        for i in range(1, Number_Steps, 1):
+            
+            vadd=(vel[i-1]+(acc[i-1]*TimeStep))
+            vel.append(vadd)
+            padd=posi[i-1]+(vel[i-1]*(TimeStep))
+            posi.append(padd)
+            aadd=(1/mass)*((-gamma*vel[i])+(dist[i]))
+            acc.append(aadd)
+            if posi[i] <= 0:
+                break
+            elif posi[i] >= 5.0:
+                break
+        print("posi")
+        print(posi)
+        l=len(posi)
+        val=l-1
+        place.append(val)
+        print("place")
+        print(place)
+        print("val")
+        print(val)
+        del posi[1:(Number_Steps-1)]
+        del vel[1:(Number_Steps-1)]
+        del acc[1:(Number_Steps-1)]
+        return place,val
+    store_placev=place.append
+    print("place")
+    print(place)
+    store_place.append(store_placev)
